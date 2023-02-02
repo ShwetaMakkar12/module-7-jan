02c3e03819b2e432dea408c0c55c1e21010e92d8
ShwetaMakkar12 <115415409+ShwetaMakkar12@users.noreply.github.com>
2023-02-02 12:31:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,42 @@
-const inputbox = document.getElementById("inputbox")
-const resultbox = document.getElementById("resultcont");
+const inputaddress = document.getElementById("inputaddress")
+const resultbox = document.getElementById("contresult");
 const error = document.getElementById("error");
-// took reference fromGeoApifi
+
+function search(){
+ const address = inputaddress.value;   
+ fetch(`https://api.geoapify.com/v1/geocode/search?text=${encodeURIComponent(address)}&apiKey=24a72da79a7e4b11ab038f850e521faf`)
+ .then(resp => resp.json())
+ .then((geocodingResult) => {
+    if(geocodingResult.features.length > 0){
+         error.classList.add("hide")
+         resultbox.classList.remove("hide")
+         searchTimeZone(geocodingResult.features[0].properties)
+        }else{
+         resultbox.classList.add("hide")
+         error.classList.remove("hide")
+        }
+    });
+    return false;
+}
+// reference took from GeoApify to of3etch using location
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
   }
 }
-
+// reference took from GeoApify tofetch using lati ang longi
 function showPosition(position) {
   fetchTimeZone(position.coords.latitude, position.coords.longitude,0);
 }
-//geoApi link to fetch the location
+// reference took from GeoApify
 function fetchTimeZone(lat,lon){
-    fetch(`https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${lon}&format=json&apiKey=24a72da79a7e4b11ab038f850e521faf
-`)
-    .then(resp => resp.json())
-    .then((result) => {
-        yourTimeZone(result.results[0],lat,lon);
+ fetch(`https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${lon}&format=json&apiKey=24a72da79a7e4b11ab038f850e521faf`)
+.then(resp => resp.json())
+.then((result) => {
+yourTimeZone(result.results[0],lat,lon);
     });
 }
-
-function searchTimeZone(data){
-    const name = document.getElementById("name-r")
-    const lati = document.getElementById("lati-r")
-    const longi = document.getElementById("longi-r")
-    const offSTD = document.getElementById("offSTD-r")
-    const offSTDsec = document.getElementById("offSTDsec-r")
-    const offDST = document.getElementById("offDST-r")
-    const offDSTsec = document.getElementById("offDSTsec-r")
-    const country = document.getElementById("country-r")
-    const city = document.getElementById("city-r")
-    name.textContent = data.timezone.name;
-    lati.textContent = data.lat;
-    longi.textContent = data.lon;
-    offSTD.textContent = data.timezone.offset_DST;
-    offSTDsec.textContent = data.timezone.offset_STD_seconds;
-    offDST.textContent = data.timezone.offset_DST;
-    offDSTsec.textContent = data.timezone.offset_DST_seconds;
-    country.textContent = data.country;
-    city.textContent = data.city;
-}
-function search(){
-    const address = inputbox.value;
-
-    fetch(`https://api.geoapify.com/v1/geocode/search?text=${encodeURIComponent(address)}&apiKey=24a72da79a7e4b11ab038f850e521faf`)
-    .then(resp => resp.json())
-    .then((geocodingResult) => {
-        if(geocodingResult.features.length > 0){
-            error.classList.add("hide")
-            resultbox.classList.remove("hide")
-            searchTimeZone(geocodingResult.features[0].properties)
-        }else{
-            resultbox.classList.add("hide")
-            error.classList.remove("hide")
-        }
-    });
-    return false;
-}
+// 
 function yourTimeZone(data,lat,lon){
     const name = document.getElementById("name")
     const lati = document.getElementById("lati")
@@ -69,6 +48,7 @@ function yourTimeZone(data,lat,lon){
     const country = document.getElementById("country")
     const city = document.getElementById("city")
     const post = document.getElementById("post")
+
     name.textContent = data.timezone.name;
     lati.textContent = lat;
     longi.textContent = lon;
@@ -79,5 +59,26 @@ function yourTimeZone(data,lat,lon){
     country.textContent = data.country;
     city.textContent = data.city;
     post.textContent = data.postcode;
+}
+function searchTimeZone(data){
+    const name = document.getElementById("name-r")
+    const lati = document.getElementById("lati-r")
+    const longi = document.getElementById("longi-r")
+    const offSTD = document.getElementById("offSTD-r")
+    const offSTDsec = document.getElementById("offSTDsec-r")
+    const offDST = document.getElementById("offDST-r")
+    const offDSTsec = document.getElementById("offDSTsec-r")
+    const country = document.getElementById("country-r")
+    const city = document.getElementById("city-r")
+
+    name.textContent = data.timezone.name;
+    lati.textContent = data.lat;
+    longi.textContent = data.lon;
+    offSTD.textContent = data.timezone.offset_DST;
+    offSTDsec.textContent = data.timezone.offset_STD_seconds;
+    offDST.textContent = data.timezone.offset_DST;
+    offDSTsec.textContent = data.timezone.offset_DST_seconds;
+    country.textContent = data.country;
+    city.textContent = data.city;
 }
 document.addEventListener("DOMContentLoaded", getLocation);
